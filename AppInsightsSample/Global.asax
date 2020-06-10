5f4ffa140ec160ce89f1517f019351aad023e836
Nick Heinbaugh <nickhein@microsoft.com>
2020-06-09 20:29:43 -0700

    Add project files.

---
+++ AppInsightsSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppInsightsSample.MvcApplication" Language="C#" %>
---
+++ AppInsightsSample/Scripts/custom/TelemetryGenerator.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+
+    $("p, div, a, input, span").click(function (e) {
+        const eName = $(this).data("event-name");
+        const eText = $(this).data("event-text");
+        if (eName) {
+            debugger;
+            e.stopPropagation();
+        }
+        debugger;
+    });
+})
